74c44936e2e8b8e3fecc1214f0d2b8416c079bec
byHYONS <byhyons@gmail.com>
2024-07-21 15:57:30 +0200

    feat: aggiunta la show page, pagina del dettaglio

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,9 +4,49 @@
 
 <div class="project-show p-5">
     <div class="container">
-        <a href="{{route('admin.projects.index') }}">{{ __('Indietro')}}"></a>
-        <div class="card p-4">
+        <div class="button-manage">
+            <div class="back">
+                <a href="{{route('admin.projects.index') }}">{{ __('Indietro')}}</a>
+            </div>
+            <div class="manage">
+                <a href="{{route('admin.projects.update', $project)}}" class="ml-45 mr-10">
+                    <i class="fas fa-pen"></i>
+                </a>
+                <a href="{{route('admin.projects.destroy', $project)}}">
+                    <i class="fas fa-trash"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="card p-5">
             <h2>{{$project->title}}</h2>
+            <hr class="mb-5">
+            <p>
+                <span>Qualit&agrave; del Progetto: </span>{{$project->project_grade}} su 10 
+                <i class="fas fa-circle"></i> 
+                <span>Categoria: </span>{{$project->market_category}}
+            </p>
+            <p>
+                <span>Materiale Creato: </span>{{$project->material_created}}
+                <i class="fas fa-circle"></i> 
+                <span>Tecnologia Usata: </span>{{$project->technologies_used}}
+            </p>
+            <p>
+                <span>Descrizione: </span>{{$project->description}}
+            </p>
+            <p>
+                <span>Collaborazione: </span>
+                {{$project->collaborations}}
+            </p>
+            <p>
+                <span>Inizio Progetto: </span>{{ Carbon::parse($project->start_project)->format('d.m.Y') }}
+                <i class="fas fa-circle"></i>
+                <span>Fine Progetto: </span>{{ Carbon::parse($project->start_project)->format('d.m.Y') }}
+            </p>
+            <p>
+                <span>Link Progetto: </span> 
+                <a class="no-btn" href="{{$project->link}}" target="_blank">{{$project->link}}</a>
+            </p>
         </div>
     </div>
 </div>
